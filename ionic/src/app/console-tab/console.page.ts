bcf6002198a2162b34c508b14e27ba7563e3a146
Noah Lanson <lanson.noah@gmail.com>
2021-04-05 16:59:01 +1000

    reorder and button examples

--- ionic/src/app/console-tab/console.page.ts
+++ ionic/src/app/console-tab/console.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IonReorderGroup } from '@ionic/angular';
+import { ItemReorderEventDetail } from '@ionic/core';
 
 @Component({
   selector: 'app-console',
@@ -7,6 +9,26 @@ import { Component } from '@angular/core';
 })
 export class ConsolePage {
 
+  public information: Array<String> = [
+    "Pizza",
+    "Sushi",
+    "Burgers"
+  ];
+
+  public counter: number = 0;
+
   constructor() {}
+
+  //requireed for ion-reorder
+  doReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    console.log('Reordered item from', ev.detail.from, 'to', ev.detail.to);
+
+    ev.detail.complete();
+  }
+
+  buttonFunction() {
+    console.log("Button was Pushed!")
+    this.counter++;
+  }
 
 }
